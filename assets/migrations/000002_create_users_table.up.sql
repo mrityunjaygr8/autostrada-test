e4791e2d812487b0b73b8cbf14e524d9c28d2362
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2023-11-01 17:04:10 +0530

    schema updated for my liking

--- assets/migrations/000002_create_users_table.up.sql
+++ assets/migrations/000002_create_users_table.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users (
-    id SERIAL NOT NULL PRIMARY KEY,
-    created TIMESTAMPTZ NOT NULL,
+    id uuid PRIMARY KEY NOT NULL ,
+    created TIMESTAMPTZ NOT NULL DEFAULT now(),
     email TEXT NOT NULL UNIQUE,
+    admin bool NOT NULL DEFAULT false,
     hashed_password TEXT NOT NULL
 );
