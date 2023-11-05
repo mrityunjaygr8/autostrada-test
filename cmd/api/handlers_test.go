6b4d62a7b6f0f4d4c4eb9739b4cfa574ac5a3e5c
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-11-05 15:47:33 +0530

    listUsers test added

--- cmd/api/handlers_test.go
+++ cmd/api/handlers_test.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/google/uuid"
 	"github.com/mrityunjaygr8/autostrada-test/store"
 	"github.com/stretchr/testify/require"
+	"math"
 	"net/http"
 	"net/http/httptest"
 	"strings"
@@ -12,167 +15,280 @@ import (
 	"time"
 )
 
-type Expectation int
-
-const (
-	Success Expectation = iota
-	FieldError
-	Error
-)
-
-type args struct {
-	input  string
-	expect string
-	code   int
-}
-
 type resp struct {
-	data        map[string]any `json:"data"`
-	fieldErrors map[string]any `json:"FieldErrors"`
-	errors      map[string]any `json:"Error"`
+	Data        map[string]any `json:"Data,omitempty"`
+	FieldErrors map[string]any `json:"FieldErrors,omitempty"`
+	Errors      string         `json:"Error,omitempty"`
 }
 
-func Test_application_createUser(t *testing.T) {
-	stubStore := NewStubStore()
-	tests := []struct {
-		name string
-		args args
-	}{
-		// TODO: Add test cases.
-		{
-			name: "basis insert success",
-			args: args{
-				input: `
-				{
-					"Email": "msyt1969@gmail.com",
-					"Password": "woowoowoo",
-					"Admin": true
-				}`,
-				code: http.StatusCreated,
-				expect: `{
-					"data": {
-						"email": "msyt1969@gmail.com",
-						"admin": true
-					}
-				}`,
-			},
-		},
-		{
-			name: "basis insert bad email",
-			args: args{
-				input: `
-				{
-					"Email": "msyt1969",
-					"Password": "woowoowoo",
-					"Admin": true
-				}`,
-				code: http.StatusUnprocessableEntity,
-				expect: `{
-					"FieldErrors": {
-						"email": "Must be a valid email address"
-					}
-				}`,
-			},
-		},
-		{
-			name: "basic insert no body",
-			args: args{
-				input:  ``,
-				code:   http.StatusBadRequest,
-				expect: `{"Error": "Body must not be empty"}`,
-			},
-		},
-	}
-	app := &application{
-		store: stubStore,
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			request := httptest.NewRequest(http.MethodPost, "/users", strings.NewReader(tt.args.input))
-			response := httptest.NewRecorder()
-			app.createUser(response, request)
-			require.Equal(t, tt.args.code, response.Code)
-			var res map[string]any
-			err := json.Unmarshal(response.Body.Bytes(), &res)
-			require.Nil(t, err)
-			var expected resp
-			t.Log(tt.args.expect, []byte(tt.args.expect))
-			err = json.Unmarshal([]byte(tt.args.expect), &expected)
-			require.Nil(t, err)
-			t.Log(expected)
-			//for key, val := range tt.args.expect {
-			//	require.Equal(t, val, res[key])
-			//}
-		})
-	}
+func IsValidUUID(u string) bool {
+	_, err := uuid.Parse(u)
+	return err == nil
 }
 
-//func Test_application_listUsers(t *testing.T) {
-//	tests := []struct {
-//		name  string
-//		store store.GuzeiStore
-//		args  args
-//	}{
-//		// TODO: Add test cases.
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			app := &application{
-//				store: tt.store,
-//			}
-//			app.listUsers(tt.args.w, tt.args.r)
-//		})
-//	}
-//}
+type createUserProps struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Admin    bool   `json:"admin"`
+}
 
-func Test_application_status(t *testing.T) {
+func TestCreateUser(t *testing.T) {
 	stubStore := NewStubStore()
-	tests := []struct {
-		name string
-		args args
-	}{
-		{
-			name: "basic status",
-			args: args{
-				code: http.StatusOK,
-				expect: `{
-					"Status": "OK",
-				}`,
-			},
-		},
-	}
 	app := &application{
-		store: stubStore,
+		store: &stubStore,
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			request := httptest.NewRequest(http.MethodGet, "/status", nil)
-			response := httptest.NewRecorder()
-			app.status(response, request)
-			var res map[string]any
-			err := json.Unmarshal(response.Body.Bytes(), &res)
+	t.Run("TestCreateUser happy path", func(t *testing.T) {
+		data := createUserProps{
+			Email:    "msyt@gmail.com",
+			Password: "qweqweqwe",
+			Admin:    false,
+		}
+		bData, err := json.Marshal(data)
+		require.Nil(t, err)
+		request := httptest.NewRequest(http.MethodPost, "/users", bytes.NewReader(bData))
+		response := httptest.NewRecorder()
+
+		app.createUser(response, request)
+
+		require.Equal(t, http.StatusCreated, response.Code)
+		var res resp
+		err = json.Unmarshal(response.Body.Bytes(), &res)
+		require.Nil(t, err)
+		require.Equal(t, data.Admin, res.Data["admin"])
+		require.Equal(t, data.Email, res.Data["email"])
+		require.True(t, IsValidUUID(res.Data["id"].(string)))
+	})
+
+	t.Run("TestCreateUser error case", func(t *testing.T) {
+		data := createUserProps{
+			Email:    "msyt",
+			Password: "qweqweqwe",
+			Admin:    false,
+		}
+		bData, err := json.Marshal(data)
+		require.Nil(t, err)
+		request := httptest.NewRequest(http.MethodPost, "/users", bytes.NewReader(bData))
+		response := httptest.NewRecorder()
+
+		app.createUser(response, request)
+
+		require.Equal(t, http.StatusUnprocessableEntity, response.Code)
+		var res resp
+		err = json.Unmarshal(response.Body.Bytes(), &res)
+		require.Nil(t, err)
+		require.Equal(t, "Must be a valid email address", res.FieldErrors["email"])
+	})
+	t.Run("TestCreateUser password too short", func(t *testing.T) {
+		data := createUserProps{
+			Email:    "msyt@gmail.com",
+			Password: "qwe",
+			Admin:    false,
+		}
+		bData, err := json.Marshal(data)
+		require.Nil(t, err)
+		request := httptest.NewRequest(http.MethodPost, "/users", bytes.NewReader(bData))
+		response := httptest.NewRecorder()
+
+		app.createUser(response, request)
+
+		require.Equal(t, http.StatusUnprocessableEntity, response.Code)
+		var res resp
+		err = json.Unmarshal(response.Body.Bytes(), &res)
+		require.Nil(t, err)
+		require.Equal(t, "Password is too short", res.FieldErrors["password"])
+	})
+	t.Run("TestCreateUser user already exists", func(t *testing.T) {
+		dataOld := createUserProps{
+			Email:    "msyt@gmail.com",
+			Password: "qweqweqwe",
+			Admin:    false,
+		}
+		bDataOld, err := json.Marshal(dataOld)
+		require.Nil(t, err)
+		requestOld := httptest.NewRequest(http.MethodPost, "/users", bytes.NewReader(bDataOld))
+		responseOld := httptest.NewRecorder()
+
+		app.createUser(responseOld, requestOld)
+		data := createUserProps{
+			Email:    "msyt@gmail.com",
+			Password: "qweqweqwe",
+			Admin:    false,
+		}
+		bData, err := json.Marshal(data)
+		require.Nil(t, err)
+		request := httptest.NewRequest(http.MethodPost, "/users", bytes.NewReader(bData))
+		response := httptest.NewRecorder()
+
+		app.createUser(response, request)
+
+		require.Equal(t, http.StatusUnprocessableEntity, response.Code)
+		var res resp
+		err = json.Unmarshal(response.Body.Bytes(), &res)
+		require.Nil(t, err)
+		require.Equal(t, "Email is already in use", res.FieldErrors["email"])
+	})
+
+	t.Run("CreateUser bad JSON", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodPost, "/users", strings.NewReader(`{"asd"`))
+		response := httptest.NewRecorder()
+
+		app.createUser(response, request)
+
+		out := `{
+	"Error": "Body contains badly-formed JSON"
+}
+`
+		require.Equal(t, http.StatusBadRequest, response.Code)
+		require.Equal(t, out, response.Body.String())
+
+	})
+}
+
+func TestListUsers(t *testing.T) {
+	t.Run("ListUsers basic case", func(t *testing.T) {
+		stubStore := NewStubStore()
+		app := &application{
+			store: &stubStore,
+		}
+		for x := 0; x < 34; x++ {
+			data := createUserProps{
+				Email:    fmt.Sprintf("msyt_%d@gmail.com", x),
+				Password: "qweqweqwe",
+				Admin:    false,
+			}
+			bData, err := json.Marshal(data)
 			require.Nil(t, err)
-			//for key, val := range tt.args.expect {
-			//	require.Equal(t, val, res[key])
-			//}
-		})
+			requestOld := httptest.NewRequest(http.MethodPost, "/users", bytes.NewReader(bData))
+			responseOld := httptest.NewRecorder()
+
+			app.createUser(responseOld, requestOld)
+
+			require.Equal(t, http.StatusCreated, responseOld.Code)
+
+		}
+		request := httptest.NewRequest(http.MethodGet, "/users", nil)
+		response := httptest.NewRecorder()
+
+		app.listUsers(response, request)
+
+		var res store.UsersList
+		err := json.Unmarshal(response.Body.Bytes(), &res)
+		require.Nil(t, err)
+		require.Equal(t, 20, res.PageSize)
+		require.Equal(t, 1, res.Page)
+		require.Equal(t, 2, res.TotalPages)
+		require.Equal(t, 34, res.TotalObjects)
+	})
+	t.Run("ListUsers query params", func(t *testing.T) {
+		stubStore := NewStubStore()
+		app := &application{
+			store: &stubStore,
+		}
+		for x := 0; x < 4; x++ {
+			data := createUserProps{
+				Email:    fmt.Sprintf("msyt_%d@gmail.com", x),
+				Password: "qweqweqwe",
+				Admin:    false,
+			}
+			bData, err := json.Marshal(data)
+			require.Nil(t, err)
+			requestOld := httptest.NewRequest(http.MethodPost, "/users", bytes.NewReader(bData))
+			responseOld := httptest.NewRecorder()
+
+			app.createUser(responseOld, requestOld)
+
+			require.Equal(t, http.StatusCreated, responseOld.Code)
+
+		}
+		pageSize := 2
+		pageNumber := 2
+		request := httptest.NewRequest(http.MethodGet, fmt.Sprintf("/users?pageNumber=%d&pageSize=%d", pageNumber, pageSize), nil)
+		response := httptest.NewRecorder()
+
+		app.listUsers(response, request)
+
+		var res store.UsersList
+		err := json.Unmarshal(response.Body.Bytes(), &res)
+		require.Nil(t, err)
+		require.Equal(t, pageSize, res.PageSize)
+		require.Equal(t, pageNumber, res.Page)
+		require.Equal(t, 4, res.TotalObjects)
+		require.Equal(t, int(math.Ceil(float64(4)/float64(pageSize))), res.TotalPages)
+		require.Equal(t, 2, len(res.Data))
+	})
+	t.Run("ListUsers query params bad page size", func(t *testing.T) {
+		stubStore := NewStubStore()
+		app := &application{
+			store: &stubStore,
+		}
+		request := httptest.NewRequest(http.MethodGet, "/users?pageSize=qwe", nil)
+		response := httptest.NewRecorder()
+
+		app.listUsers(response, request)
+		out := `{
+	"FieldErrors": {
+		"pageSize": "pageSize must be a positive integer"
 	}
+}
+`
+
+		require.Equal(t, http.StatusUnprocessableEntity, response.Code)
+		require.Equal(t, out, response.Body.String())
+	})
+	t.Run("ListUsers query params bad page number", func(t *testing.T) {
+		stubStore := NewStubStore()
+		app := &application{
+			store: &stubStore,
+		}
+		request := httptest.NewRequest(http.MethodGet, "/users?pageNumber=-123", nil)
+		response := httptest.NewRecorder()
+
+		app.listUsers(response, request)
+		out := `{
+	"FieldErrors": {
+		"pageNumber": "pageNumber must be a positive integer"
+	}
+}
+`
+
+		//t.Log(response.Body.String())
+		require.Equal(t, http.StatusUnprocessableEntity, response.Code)
+		require.Equal(t, out, response.Body.String())
+	})
+}
+
+func TestStatus(t *testing.T) {
+	stubStore := NewStubStore()
+	app := &application{
+		store: &stubStore,
+	}
+
+	t.Run("Status check", func(t *testing.T) {
+		request := httptest.NewRequest(http.MethodPost, "/status", nil)
+		response := httptest.NewRecorder()
+
+		app.status(response, request)
+
+		require.Equal(t, http.StatusOK, response.Code)
+		out := `{
+	"Status": "OK"
+}
+`
+		require.Equal(t, out, response.Body.String())
+
+	})
 }
 
 type StubStore struct {
-	userStore map[string]store.User
+	userStore []store.User
 }
 
 func NewStubStore() StubStore {
-	userStore := make(map[string]store.User)
+	userStore := make([]store.User, 0)
 	return StubStore{userStore}
 }
 
-func (s StubStore) UserInsert(email, password string, id uuid.UUID, admin bool) (*store.User, error) {
-	_, found := s.userStore[email]
-	if found {
-		return nil, store.ErrUserExists
-	}
+func (s *StubStore) UserInsert(email, password string, id uuid.UUID, admin bool) (*store.User, error) {
 	u := store.User{
 		Email:          email,
 		ID:             id,
@@ -180,39 +296,53 @@ func (s StubStore) UserInsert(email, password string, id uuid.UUID, admin bool) 
 		Created:        time.Now(),
 		HashedPassword: password,
 	}
-	s.userStore[email] = u
+	s.userStore = append(s.userStore, u)
 	return &u, nil
 }
 
-func (s StubStore) UserList(userListParams store.UserListParams) (*store.UsersList, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (s StubStore) UserRetrieveByEmail(email string) (*store.User, error) {
-	u, found := s.userStore[email]
-	if !found {
-		return nil, store.ErrUserNotFound
+func (s *StubStore) UserList(userListParams store.UserListParams) (*store.UsersList, error) {
+	users := make([]store.User, 0)
+	count := len(s.userStore)
+	start := userListParams.PageSize * (userListParams.PageNumber - 1)
+	for _, val := range s.userStore[start : start+userListParams.PageSize] {
+		users = append(users, val)
 	}
-	return &u, nil
+	res := store.UsersList{
+		Data:         users,
+		TotalObjects: count,
+		TotalPages:   int(math.Ceil(float64(count) / float64(userListParams.PageSize))),
+		Page:         userListParams.PageNumber,
+		PageSize:     userListParams.PageSize,
+	}
+
+	return &res, nil
 }
 
-func (s StubStore) UserRetrieve(id uuid.UUID) (*store.User, error) {
+func (s *StubStore) UserRetrieveByEmail(email string) (*store.User, error) {
+	for _, item := range s.userStore {
+		if item.Email == email {
+			return &item, nil
+		}
+	}
+	return nil, store.ErrUserNotFound
+}
+
+func (s *StubStore) UserRetrieve(id uuid.UUID) (*store.User, error) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (s StubStore) UserUpdatePassword(id uuid.UUID, newPassword string) error {
+func (s *StubStore) UserUpdatePassword(id uuid.UUID, newPassword string) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (s StubStore) UserUpdateAdmin(id uuid.UUID, newAdminValue bool) error {
+func (s *StubStore) UserUpdateAdmin(id uuid.UUID, newAdminValue bool) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (s StubStore) UserDelete(id uuid.UUID) error {
+func (s *StubStore) UserDelete(id uuid.UUID) error {
 	//TODO implement me
 	panic("implement me")
 }
